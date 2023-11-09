4869c5ac3718f6b05a5983911803cb296f70d06d
Rick <S1204255@student.windesheim.nl>
2023-11-09 18:19:47 +0100

    Auto stash before checking out "HEAD"

--- _includes/listspage.md
+++ _includes/listspage.md
@@ -1,9 +1,9 @@
 <div id="lists" class="content-wrapper">
     <button id="lists-personal-button" onclick="window.location.href='personal/homepage'">Personal Lists</button>
     <div class="wrapper">
-        <a href="list/movielist" id="movieslist">Movie list</a>
-        <a href="list/showslist" id="showslist">Shows list</a>
-        <a href="list/gameslist" id="gameslist">Games list</a>
+        <a href="list/movielist" id="movieslist"><h1>Movie list</h1></a>
+        <a href="list/showslist" id="showslist"><h1>Shows list</h1></a>
+        <a href="list/gameslist" id="gameslist"><h1>Games list</h1></a>
     </div>
     <button id="lists-back-button">Back</button>
 </div>
--- scripts/protectedpage.js
+++ scripts/protectedpage.js
@@ -19,29 +19,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-function promptForPassword() {
+async function promptForPassword() {
     // When user logs in
     var userPassword;
-    var checkPasswordInterval = setInterval(function () {
+    var checkPasswordInterval = setInterval(async function () {
         userPassword = prompt('Please enter the password:');
         if (userPassword === null) {
             // User clicked "Cancel", redirect to '/lists'
             window.location.href = '/lists';
             clearInterval(checkPasswordInterval);
-        } else if (btoa(userPassword) === 'cGFzc3dvcmQ=') { // 'password' encoded in Base64
-            // Save logged in status
-            sessionStorage.setItem('isLoggedIn', 'true');
-            // Stop checking the password
-            clearInterval(checkPasswordInterval);
-            // Show the content
-            if (document.getElementById('personallists')) {
-                document.getElementById('personallists').style.display = 'flex';
-            } else if (document.getElementById('personallist')) {
-                document.getElementById('personallist').style.display = 'block';
-            }
         } else {
-            alert('Incorrect password');
-            // The page will not reload, but the prompt will show again
+            // Hash the user's password
+            const msgUint8 = new TextEncoder().encode(userPassword);
+            const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hashedPassword = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+            if (hashedPassword === '483029d526219f816e8e8f6a9de07b422633dba180ffc26faac22862a017519f') { // '2004' hashed with SHA-256
+                // Save logged in status
+                sessionStorage.setItem('isLoggedIn', 'true');
+                // Stop checking the password
+                clearInterval(checkPasswordInterval);
+                // Show the content
+                if (document.getElementById('personallists')) {
+                    document.getElementById('personallists').style.display = 'flex';
+                } else if (document.getElementById('personallist')) {
+                    document.getElementById('personallist').style.display = 'block';
+                }
+            } else {
+                alert('Incorrect password');
+                // The page will not reload, but the prompt will show again
+            }
         }
     }, 100); // Check the password every 100 milliseconds
 }
