87eaa2e86bd65b03f617987e4befc4d384eb8a58
Rick <S1204255@student.windesheim.nl>
2023-11-18 22:56:53 +0100

    update homepage

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,4 +1,4 @@
-var text = document.getElementById('animated-text');
+var text = document.getElementById("animated-text");
 var word = text.textContent;
 var counter = 0;
 var direction = 1;
@@ -6,53 +6,73 @@ var isTyping = true;
 var isDeleting = false;
 var cursorVisible = true;
 var blinkInterval;
+var typeInterval;
 
 // Add the cursor span to the HTML once
-var cursor = document.createElement('span');
-cursor.id = 'cursor';
-cursor.className = 'cursor-visible';
-cursor.textContent = '|';
+var cursor = document.createElement("span");
+cursor.id = "cursor";
+cursor.className = "cursor-visible";
+cursor.textContent = "|";
 text.appendChild(cursor);
 
 function typeAndCursor() {
-    if (counter === word.length && isTyping) {
-        isTyping = false;
-        setTimeout(() => {
-            isDeleting = true;
-        }, 2000);
-    }
+  if (counter === word.length && isTyping) {
+    isTyping = false;
+    setTimeout(() => {
+      isDeleting = true;
+    }, 2000);
+  }
 
-    if (counter === 0 && isDeleting) {
-        isDeleting = false;
-        setTimeout(() => {
-            isTyping = true;
-        }, 2000);
-        direction = 1;
-    }
+  if (counter === 0 && isDeleting) {
+    isDeleting = false;
+    setTimeout(() => {
+      isTyping = true;
+    }, 2000);
+    direction = 1;
+  }
 
-    if (isTyping) {
-        counter += direction;
-    } else if (isDeleting) {
-        counter -= direction;
-    }
+  if (isTyping) {
+    counter += direction;
+  } else if (isDeleting) {
+    counter -= direction;
+  }
 
-    // Use textContent to update the text without affecting the cursor
-    text.textContent = word.substring(0, counter);
-    text.appendChild(cursor);
+  // Use textContent to update the text without affecting the cursor
+  text.textContent = word.substring(0, counter);
+  text.appendChild(cursor);
 
-    // If text is not being typed or deleted, start blinking
-    if (!isTyping && !isDeleting && !blinkInterval) {
-        blinkInterval = setInterval(blinkCursor, 500);
-    } else if ((isTyping || isDeleting) && blinkInterval) {
-        clearInterval(blinkInterval);
-        blinkInterval = null;
-        cursor.className = 'cursor-visible';
-    }
+  // If text is not being typed or deleted, start blinking
+  if (!isTyping && !isDeleting && !blinkInterval) {
+    blinkInterval = setInterval(blinkCursor, 500);
+  } else if ((isTyping || isDeleting) && blinkInterval) {
+    clearInterval(blinkInterval);
+    blinkInterval = null;
+    cursor.className = "cursor-visible";
+  }
 }
 
 function blinkCursor() {
-    cursorVisible = !cursorVisible;
-    cursor.className = cursorVisible ? 'cursor-visible' : 'cursor-invisible';
+  cursorVisible = !cursorVisible;
+  cursor.className = cursorVisible ? "cursor-visible" : "cursor-invisible";
 }
 
-setInterval(typeAndCursor, 200);
+// Animation initialization function
+window.initializeAnimations = function () {
+  // Reset or initialize your animation state here
+  counter = 0;
+  direction = 1;
+  isTyping = true;
+  isDeleting = false;
+  cursorVisible = true;
+  if (blinkInterval) {
+    clearInterval(blinkInterval);
+    blinkInterval = null;
+  }
+  if (typeInterval) {
+    clearInterval(typeInterval);
+  }
+  typeInterval = setInterval(typeAndCursor, 200);
+};
+
+// Initialize animations when the script is loaded
+window.initializeAnimations();
