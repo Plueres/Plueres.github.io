306f5d88c3f064ae117f47090c8c60a07a67b662
Rick <S1204255@student.windesheim.nl>
2024-02-08 23:04:44 +0100

    Add script.js to default.html and remove service worker registration from global.js

--- scripts/global.js
+++ scripts/global.js
@@ -98,16 +98,3 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.style.display = "none";
     }
 });
-
-// SERVICE WORKER
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
