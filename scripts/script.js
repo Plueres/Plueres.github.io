0dc0f7861738ecfa6673f9914561049ccf16cca3
Rick <S1204255@student.windesheim.nl>
2024-02-09 15:13:18 +0100

    tried jekyll-pwa-plugin to Gemfile.lock and remove console.log from script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,3 @@
-// SERVICE WORKER
-console.log('Script is running');
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
+    navigator.serviceWorker.register('/service-worker.js');
 }
